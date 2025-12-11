a85c2b804229220ec90cb2cc8760e84d43c41538
omkale07 <okale753@gmail.com>
2025-12-11 12:27:22 +0530

    updated

--- news.js
+++ news.js
@@ -1,4 +1,26 @@
 function  toggleDark(){
     document.body.classList.toggle("dark");
-    console.log("toggleclick")
+    console.log("toggleclick");
 }
+
+
+const search = document.getElementById("search");
+const cards = document.querySelectorAll(".card");
+
+search.addEventListener("input", ()=>{
+  const text = search.value.toLowerCase();
+
+    cards.forEach(card =>{
+        const title = card.querySelector(".card-title").innerText.toLowerCase();
+        const desc = card.querySelector(".card-text").innerHTML.toLocaleLowerCase();
+
+        if(title.includes(text) || desc.includes(text)){
+            console.log("found");
+            card.style.display = "block";
+        }
+        else{
+            console.log(" not found");
+            card.style.display = "none";
+        }
+    })
+})
